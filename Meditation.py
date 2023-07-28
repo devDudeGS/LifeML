ced4188ef24396216b0ccb053cdb482738789f1e
Gregory Stevens <networkwithgreg@gmail.com>
2023-07-27 21:41:07 -0400

    Changed model to Lasso to handle categorical data

--- Meditation.py
+++ Meditation.py
@@ -1,20 +1,27 @@
 import pandas as pd
-from sklearn.linear_model import LinearRegression
+from sklearn.linear_model import LinearRegression, Lasso
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import mean_squared_error
 import matplotlib.pyplot as plt
 
 
 """
 Used GitHub Copilot and Claude 2
 
-Most important numerical features for me:
+LR important numerical features for me:
 1. total_meditation_mins
 2. consecutive_meditation_days
 3. active_zone_mins_today
 
-Most important categorical features for me:
+LR important categorical features for me:
 1. diet_yesterday_1
 2. diet_today_2
 3. caffeine_0
+
+Lasso important features for me:
+1. active_zone_mins_today
+2. first_meditation_time
+3. sleep_length_hrs
 """
 
 # Read in data from csv file
@@ -28,40 +35,73 @@ df.drop(['inability_to_cope_with_responsibilities (0-4)', 'feeling_score_fitbit_
          'meditation_type (cat)'], axis=1, inplace=True)
 
 # Drop categorical columns
-df.drop(['diet_today (0 or 1 or 2)', 'diet_yesterday (0 or 1 or 2)', 'alcohol_today (0 or 1)',
-         'alcohol_yesterday (0 or 1)', 'caffeine (0 or 1)',], axis=1, inplace=True)
+# df.drop(['diet_today (0 or 1 or 2)', 'diet_yesterday (0 or 1 or 2)', 'alcohol_today (0 or 1)',
+#          'alcohol_yesterday (0 or 1)', 'caffeine (0 or 1)',], axis=1, inplace=True)
 
 # Encode categorical columns
-# df = pd.get_dummies(df, columns=['diet_today (0 or 1 or 2)', 'diet_yesterday (0 or 1 or 2)',
-#                                  'alcohol_today (0 or 1)', 'alcohol_yesterday (0 or 1)', 'caffeine (0 or 1)'])
+df = pd.get_dummies(df, columns=['diet_today (0 or 1 or 2)', 'diet_yesterday (0 or 1 or 2)',
+                                 'alcohol_today (0 or 1)', 'alcohol_yesterday (0 or 1)', 'caffeine (0 or 1)'])
 
 # Convert data to numeric float values
 df = df.astype('float')
 
-# Split data into X (features) and y (target)
+# # Split data into X (features) and y (target)
 X = df.drop('target_feelings', axis=1)
 y = df['target_feelings']
 
-# Create linear regression model
-lr = LinearRegression()
+# Linear Regression
+# # Create linear regression model
+# lr = LinearRegression()
 
-# Fit model to data
-lr.fit(X, y)
+# # Fit model to data
+# lr.fit(X, y)
 
-# Print model coefficients
-print("Coefficients:", lr.coef_)
+# # Print model coefficients
+# print("Coefficients:", lr.coef_)
 
-# Print R-squared score
-print("R-squared score:", lr.score(X, y))
+# # Print R-squared score
+# print("R-squared score:", lr.score(X, y))
+
+# Split the data into training and testing sets
+X_train, X_test, y_train, y_test = train_test_split(
+    X, y, test_size=0.2, random_state=42)
+
+# needed?
+# X_train, X_val, y_train, y_val = train_test_split(
+#     X_train, y_train, test_size=0.25, random_state=42)
+
+# alphas = [1, 0.8, 0.5, 0.3, 0.1, 0.08, 0.05, 0.03, 0.01]
+
+# Lasso Regression
+# Create a Lasso regression object
+lasso = Lasso(alpha=1.0)
+
+# Fit the model to your data
+lasso.fit(X_train, y_train)
+
+# for a in alphas:
+#     print('Lasso Regression')
+#     print('alpha:', a)
+#     model = Lasso(alpha=a)
+#     model.fit(X_train, y_train)
+#     y_pred = model.predict(X_val)
+#     rmse = mean_squared_error(y_val, y_pred, squared=False)
+#     print('RMSE:', rmse)
+
+# Evaluate the model on your test data
+score = lasso.score(X_test, y_test)
+print('Lasso regression score:', score)
 
 # Extract the feature names
-feature_names = X.columns
+# feature_names = X.columns
+feature_names = X_train.columns
 
 # Get the coefficients from the model
-coefficients = lr.coef_
+# coefficients = lr.coef_
+coefficients = lasso.coef_
 
-# Round the coefficients to 5 decimal places
-coefficients = [round(coefficient, 5) for coefficient in coefficients]
+# Round the coefficients to 6 decimal places
+coefficients = [round(coefficient, 6) for coefficient in coefficients]
 
 # Combine to dataframe
 coef_df = pd.DataFrame(
@@ -72,6 +112,13 @@ coef_df = coef_df.sort_values(by='Coefficient', ascending=False)
 
 # Print results
 print(coef_df)
+print()
+print("Legend")
+print("High positive coefficient = strong positive relationship")
+print("Low positive coefficient = weak positive relationship")
+print("High negative coefficient = strong negative relationship")
+print("Low negative coefficient = weak negative relationship")
+print("Coefficient near 0 = little linear relationship")
 
 # # Plot bar chart
 # plt.bar(feature_names, coefficients)
--- Sleep.py
+++ Sleep.py
@@ -1,22 +1,27 @@
 import pandas as pd
-from sklearn.linear_model import LinearRegression
+from sklearn.linear_model import LinearRegression, Lasso
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import mean_squared_error
 import matplotlib.pyplot as plt
 
 
 """
 Used GitHub Copilot and Claude 2
 
-Most important numerical features for me:
+LR important numerical features for me:
 1. consecutive days met
 2. adj time awake
 3. sleep score / zone minutes yesterday
 
-time asleep ?? (#2 when categorical features incl)
-
-Most important categorical features for me:
+LR important categorical features for me:
 1. NOT diet yesterday (cat 0 or 1 or 2)_0
 2. goal met (cat 0 or 1)_0
 3. diet yesterday (cat 0 or 1 or 2)_1
+
+Lasso important features for me:
+1. wake up diff (7:30)
+2. zone minutes yesterday
+3. LESS bedtime diff (22:30)
 """
 
 # Read in data from csv file
@@ -25,7 +30,7 @@ df = pd.read_csv('data/sleep.csv', index_col=0)
 
 # Drop unnecessary columns
 df.drop(['how awesome (0-4)', 'how unneeded caffeine (0-4)',
-        'other notes', 'goal'], axis=1, inplace=True)
+        'other notes', 'goal', 'time awake'], axis=1, inplace=True)
 
 # Drop categorical columns
 # df.drop(['diet yesterday (cat 0 or 1 or 2)', 'meditated yesterday (cat 0 or 1)', 'alcohol yesterday (cat 0 or 1)',
@@ -39,30 +44,62 @@ df = pd.get_dummies(df, columns=['diet yesterday (cat 0 or 1 or 2)', 'meditated 
 # Convert data to numeric float values
 df = df.astype('float')
 
-# Split data into X (features) and y (target)
-X = df.drop('target', axis=1)
-y = df['target']
+# Linear Regression
+# # Split data into X (features) and y (target)
+# X = df.drop('target', axis=1)
+# y = df['target']
 
-# Create linear regression model
-lr = LinearRegression()
+# # Create linear regression model
+# lr = LinearRegression()
 
-# Fit model to data
-lr.fit(X, y)
+# # Fit model to data
+# lr.fit(X, y)
 
-# Print model coefficients
-print("Coefficients:", lr.coef_)
+# # Print model coefficients
+# print("Coefficients:", lr.coef_)
 
-# Print R-squared score
-print("R-squared score:", lr.score(X, y))
+# # Print R-squared score
+# print("R-squared score:", lr.score(X, y))
+
+# Split the data into training and testing sets
+X_train, X_test, y_train, y_test = train_test_split(
+    df.drop('target', axis=1), df['target'], test_size=0.2, random_state=42)
+
+# # needed?
+# X_train, X_val, y_train, y_val = train_test_split(
+#     X_train, y_train, test_size=0.25, random_state=42)
+
+# alphas = [1, 0.8, 0.5, 0.3, 0.1, 0.08, 0.05, 0.03, 0.01]
+
+# Lasso Regression
+# Create a Lasso regression object
+lasso = Lasso(alpha=1.0)
+
+# Fit the model to your data
+lasso.fit(X_train, y_train)
+
+# for a in alphas:
+#     print('alpha:', a)
+#     model = Lasso(alpha=a)
+#     model.fit(X_train, y_train)
+#     y_pred = model.predict(X_val)
+#     rmse = mean_squared_error(y_val, y_pred, squared=False)
+#     print('RMSE:', rmse)
+
+# Evaluate the model on your test data
+score = lasso.score(X_test, y_test)
+print('Lasso regression score:', score)
 
 # Extract the feature names
-feature_names = X.columns
+# feature_names = X.columns
+feature_names = X_train.columns
 
 # Get the coefficients from the model
-coefficients = lr.coef_
+# coefficients = lr.coef_
+coefficients = lasso.coef_
 
-# Round the coefficients to 5 decimal places
-coefficients = [round(coefficient, 5) for coefficient in coefficients]
+# Round the coefficients to 6 decimal places
+coefficients = [round(coefficient, 6) for coefficient in coefficients]
 
 # Combine to dataframe
 coef_df = pd.DataFrame(
@@ -73,6 +110,13 @@ coef_df = coef_df.sort_values(by='Coefficient', ascending=False)
 
 # Print results
 print(coef_df)
+print()
+print("Legend")
+print("High positive coefficient = strong positive relationship")
+print("Low positive coefficient = weak positive relationship")
+print("High negative coefficient = strong negative relationship")
+print("Low negative coefficient = weak negative relationship")
+print("Coefficient near 0 = little linear relationship")
 
 # # Plot bar chart
 # plt.bar(feature_names, coefficients)
