786fb43a4813ba54ebfebf4d36b9485a91577ae2
Gregory Stevens <networkwithgreg@gmail.com>
2023-08-06 16:33:17 -0400

    Refactored csv_dataset based on PyCharm recs

--- csv_dataset.py
+++ csv_dataset.py
@@ -8,6 +8,8 @@ class CsvDataset:
         Initialize a dataframe from a CSV file.
         """
         self.df = pd.read_csv(file_path, index_col=0)
+        self.X = None
+        self.y = None
 
     def drop_columns(self, columns):
         """
@@ -29,32 +31,34 @@ class CsvDataset:
         self.X = self.df.drop(target_column, axis=1)
         self.y = self.df[target_column]
 
-    def run_model(self, X, y):
-        """
-        Run the ML model on the dataset, and return the coefficients and feature names.
-        """
-        model = LassoRegression(alpha=1.0)
-        model.split_data(X, y)
-        best_a = model.get_best_alpha(
-            model.X_train, model.y_train, model.X_val, model.y_val)
-        model.set_alpha(best_a)
-        model.fit(model.X_train, model.y_train)
-        model.score(model.X_test, model.y_test)
-        return model.get_coefficients_and_features(X)
 
-    def print_results(self, coefficients, feature_names):
-        """
-        Print the results of the ML model.
-        """
-        coef_df = pd.DataFrame(
-            data=coefficients, index=feature_names, columns=['Coefficient'])
-        coef_df = coef_df.sort_values(by='Coefficient', ascending=False)
-        print(coef_df)
-        print()
-        print("Legend")
-        print("High positive coefficient = strong positive relationship")
-        print("Low positive coefficient = weak positive relationship")
-        print("High negative coefficient = strong negative relationship")
-        print("Low negative coefficient = weak negative relationship")
-        print("Coefficient near 0 = little linear relationship")
-        print()
+def run_model(X, y):
+    """
+    Run the ML model on the dataset, and return the coefficients and feature names.
+    """
+    model = LassoRegression(alpha=1.0)
+    model.split_data(X, y)
+    best_a = model.get_best_alpha(
+        model.X_train, model.y_train, model.X_val, model.y_val)
+    model.set_alpha(best_a)
+    model.fit(model.X_train, model.y_train)
+    model.score(model.X_test, model.y_test)
+    return model.get_coefficients_and_features(X)
+
+
+def print_results(coefficients, feature_names):
+    """
+    Print the results of the ML model.
+    """
+    coef_df = pd.DataFrame(
+        data=coefficients, index=feature_names, columns=['Coefficient'])
+    coef_df = coef_df.sort_values(by='Coefficient', ascending=False)
+    print(coef_df)
+    print()
+    print("Legend")
+    print("High positive coefficient = strong positive relationship")
+    print("Low positive coefficient = weak positive relationship")
+    print("High negative coefficient = strong negative relationship")
+    print("Low negative coefficient = weak negative relationship")
+    print("Coefficient near 0 = little linear relationship")
+    print()
