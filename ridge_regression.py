86b17dbccbaf8032b8f310c54103bb07ef36426e
Gregory Stevens <networkwithgreg@gmail.com>
2023-08-19 15:46:26 -0400

    Added RidgeRegression as another model

--- ridge_regression.py
+++ ridge_regression.py
@@ -1,22 +1,23 @@
-from sklearn.linear_model import Lasso
+from sklearn.linear_model import Ridge
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import mean_squared_error
 import numpy as np
+from sklearn.metrics import r2_score
 
 
-class LassoRegression:
+class RidgeRegression:
     def __init__(self, alpha=1.0):
         """
-        Initialize a LassoRegression object with the specified alpha value.
+        Initialize a RidgeRegression object with the specified alpha value.
         """
         self.set_alpha(alpha)
 
     def set_alpha(self, alpha):
         """
-        Set an alpha value on a LassoRegression object.
+        Set an alpha value on a RidgeRegression object.
         """
         self.alpha = float(alpha)
-        self.model = Lasso(alpha=self.alpha)
+        self.model = Ridge(alpha=self.alpha)
 
     def split_data(self, X, y):
         """
@@ -36,45 +37,59 @@ class LassoRegression:
 
     def get_best_alpha(self, X_train, y_train, X_val, y_val):
         """
-        Find the best alpha for the Lasso regression model.
+        Find the best alpha value for the Ridge regression model.
         """
         alphas = [1, 0.8, 0.5, 0.3, 0.1, 0.08, 0.05, 0.03, 0.01]
-        best_rmse = float(np.inf)
+        best_score = float(-np.inf)
         best_alpha = None
         for alpha in alphas:
-            model = Lasso(alpha=alpha)
+            model = Ridge(alpha=alpha)
             model.fit(X_train, y_train)
 
-            y_pred = model.predict(X_val)
-            rmse = np.sqrt(mean_squared_error(y_val, y_pred))
-            if rmse < best_rmse:
-                best_rmse = rmse
+            score = model.score(X_val, y_val)
+            if score > best_score:
+                best_score = score
                 best_alpha = alpha
 
         print('Best alpha:', best_alpha)
-        print('Best RMSE (lower is better):', best_rmse)
+        print('Best R-squared score (higher is better)', best_score)
         print()
         return best_alpha
 
     def fit(self, X_train, y_train):
         """
-        Fit the Lasso regression model to the specified training data.
+        Fit the Ridge regression model to the specified training data.
         """
         self.model.fit(X_train, y_train)
         return self
 
-    def score(self, X_test, y_test):
+    def score(self, X, y):
         """
-        Get the R-squared score for the Lasso regression model on the specified test data.
+        Calculate the R-squared score for the model.
         """
-        score = self.model.score(X_test, y_test)
-        print('R-squared score:', score)
-        print()
+        y_pred = self.predict(X)
+        score = r2_score(y, y_pred)
         return score
+
+    def predict(self, X):
+        """
+        Predict the target variable for the specified input data.
+        """
+        return self.model.predict(X)
+
+    def calculate_rmse(self, X, y):
+        """
+        Calculate the RMSE for the model.
+        """
+        y_pred = self.predict(X)
+        rmse = np.sqrt(mean_squared_error(y, y_pred))
+        print('RMSE:', rmse)
+        print()
+        return rmse
 
     def get_coefficients_and_features(self, X):
         """
-        Get the coefficients and feature names for the Lasso regression model.
+        Get the coefficients and feature names for the Ridge regression model.
         Returns a tuple containing two lists: the coefficients and the feature names.
         """
         coefficients = self.model.coef_
